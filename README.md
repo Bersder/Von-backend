c71092ce2f10f3e81b3f4cd914c309d8809564a1
Bersder3000 <B1747322151@outlook.com>
2019-10-08 16:58:04 +0800

    log02

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # nameless-blog_apis
 back-end of nameless-blog
 
-# Log
-修复&新增
-## log01
+## 接口详情
+### 授权接口
+需要登录后使用
+### 普通接口
+没有限制，大部分是读操作
+
+
+## Log
+修复&新增,(SP为重要保存点)
+### log01 **(SP评论表去冗余前)**
 - [X] apiv1：note接口新增notice信息
 - [X] 创建Series页面的接口apiv14
 - [X] apiv7：评论接口新增机器人验证（简单数学题）
 - [X] v10api：新系列的创建与要添加描述
 - [X] mdimg：新增图片删除接口
+### log02 
+- [X] apiv6&apiv7：评论表去冗余
--- apiv6.php
+++ apiv6.php
@@ -20,13 +20,32 @@ if(isset($_POST['type'])&&in_array($_POST['type'],['anime','code','game','trivia
         $id = $_POST['id'];
         $offset = isset($_POST['offset'])?intval($_POST['offset']):0;
 
-        $allCount = mysqli_fetch_row(maria($link,"select count(id) from Comment.comment where topic_id=$id and topic_type='$type'"))[0];
-        $commentCount = mysqli_fetch_row(maria($link,"select count(id) from Comment.comment where topic_id=$id and topic_type='$type' and parent_id is null"))[0];
+        $allCount = mysqli_fetch_row(maria($link,"select count(id) from Comment.comment where topic_id=$id and topic_type='$type'"))[0];//评论（含回复）
+        $commentCount = mysqli_fetch_row(maria($link,"select count(id) from Comment.comment where topic_id=$id and topic_type='$type' and parent_id is null"))[0];//主评论
         $comments = [];
         $allReplies = [];
-        $res = maria($link,"select * from Comment.comment where topic_id=$id and topic_type='$type' and parent_id is null order by datetime desc limit $offset,10");
+        //select * from Comment.comment where topic_id=$id and topic_type='$type' and parent_id is null order by datetime desc limit $offset,10
+        $res = maria($link,"
+        select * from
+            (select c1.id as id,uid,avatar,ulink,uname,to_uid,parent_id,c1.datetime as datetime,content
+             from Comment.comment as c1,User.user as u1
+             where topic_id=$id and topic_type='$type' and parent_id is null and c1.uid=u1.id) as c2
+            left join
+            (select id as to_uid,uname as to_uname from User.user) as u2
+            using(to_uid)
+            order by datetime desc limit $offset,10
+        ");
         while ($each = mysqli_fetch_assoc($res))$comments[] = $each;
-        $res = maria($link,"select * from Comment.comment where topic_id=$id and topic_type='$type' and parent_id is not null");
+        //select * from Comment.comment where topic_id=$id and topic_type='$type' and parent_id is not null
+        $res = maria($link,"
+        select * from
+            (select c1.id as id,uid,avatar,ulink,uname,to_uid,parent_id,c1.datetime as datetime,content
+             from Comment.comment as c1,User.user as u1
+             where topic_id=$id and topic_type='$type' and parent_id is not null and c1.uid=u1.id) as c2
+            left join
+            (select id as to_uid,uname as to_uname from User.user) as u2
+            using(to_uid)
+        ");
         while ($each = mysqli_fetch_assoc($res))$allReplies[] = $each;
         gen_replies($comments);
         echo json_encode(['code'=>0,'data'=>['comments'=>$comments,'allCount'=>$allCount,'commentCount'=>$commentCount]]);
