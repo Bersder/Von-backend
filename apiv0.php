9dca0e346353726e8ec08ff0c4133b07ca864e69
Bersder3000 <B1747322151@outlook.com>
2020-04-01 13:44:53 +0800

    v1.2.0

--- apiv0.php
+++ apiv0.php
@@ -1,5 +1,5 @@
 <?php //侧边导航栏初始化
 require 'utils/init.php';
 require 'links/public_link.php';
-$info =  mysqli_fetch_assoc(maria($link,"select sign,status from User.me limit 1"));
+$info =  mysqli_fetch_assoc(maria($link,"select name,avatar,sign,status from User.me limit 1"));
 echo json_encode(['code'=>0,'data'=>['info'=>$info]],JSON_NUMERIC_CHECK);
